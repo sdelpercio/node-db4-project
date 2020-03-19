7e387db8b9e1c986e4ba8dbf69108646e9eb922a
Shawn DelPercio <sdelperc@gmail.com>
2020-03-19 16:13:47 -0400

    added knex schema to create all tables

--- data/migrations/20200319155026_bootstrap.js
+++ data/migrations/20200319155026_bootstrap.js
@@ -1,8 +1,48 @@
-
 exports.up = function(knex) {
-  
+	return knex.schema
+		.createTable('recipes', tbl => {
+			tbl.increment();
+			tbl.string('name', 255).notNullable();
+			tbl.string('cook_time', 255);
+			tbl.string('difficulty', 255);
+		})
+		.createTable('ingredients', tbl => {
+			tbl.increment();
+			tbl.string('name', 255).notNullable();
+			tbl.string('type', 255);
+		})
+		.createTable('recipe-ingredients', tbl => {
+			tbl
+				.integer('recipe_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('recipes')
+				.onDelete('RESTRICT')
+				.onUpdate('CASCADE');
+			tbl
+				.integer('ingredient_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('ingredients')
+				.onDelete('RESTRICT')
+				.onUpdate('CASCADE');
+			tbl.string('amount', 255);
+		})
+		.createTable('instructions', tbl => {
+			tbl.increment();
+			tbl.integer('step').notNullable();
+			tbl.text('description').notNullable();
+			tbl
+				.integer('recipe_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('recipes')
+				.onDelete('RESTRICT')
+				.onUpdate('CASCADE');
+		});
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function(knex) {};
